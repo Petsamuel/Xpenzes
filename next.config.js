7fd9f1a84a31cfcacfb2ca83bb73c2ebdab604d9
petsamuel <petsamuel4@gmail.com>
2023-10-23 00:40:59 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-   headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-        ],
-      },
-    ];
-  },
-} = nextConfig;
-
+module.exports = nextConfig;
