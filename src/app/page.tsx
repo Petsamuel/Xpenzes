d5b35c61687537c1be1a970abb0c3fce9aba0709
petsamuel <petsamuel4@gmail.com>
2023-10-22 23:44:44 +0100

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { DownloadButton } from "./components/Button";
 import { AlertMessage } from "./components/AlertMessage";
-// import html2PDF from "jspdf-html2canvas";
 import { UserAuth } from "./context/AuthContext";
 import dynamic from "next/dynamic";
 
@@ -159,12 +158,12 @@ export default function Home() {
           format: "a4",
         },
         imageType: "image/jpeg",
-        output: "./pdf/generate.pdf",
+        output: "https://xpenzes.vercel.app/Xpenze.pdf",
       });
       setAlert({ message: "File downloaded", status: 200 });
       const timer = setTimeout(() => {
         setAlert({ message: "", status: 0 });
-      }, 300);
+      }, 500);
       return () => {
         clearTimeout(timer);
       };
@@ -173,7 +172,7 @@ export default function Home() {
 
       const timer = setTimeout(() => {
         setAlert({ message: "", status: 0 });
-      }, 300);
+      }, 500);
       return () => {
         clearTimeout(timer);
       };
@@ -303,28 +302,32 @@ export default function Home() {
               {/* download button */}
             </div>
           )}
-          {list.length != 0 && <DownloadButton handleEvent={DownloadFile} />}
+          {list.length != 0 && user && (
+            <DownloadButton handleEvent={DownloadFile} />
+          )}
         </div>
 
         {/*  */}
         <div className="fixed bottom-6 right-4 flex space-y-2 flex-col gap-4 items-center">
           <div className="" title="Visit Github">
-            <svg
-              stroke="currentColor"
-              fill="none"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="text-lg text-gray2 hover:text-black1 dark:hover:text-white cursor-pointer"
-              height="1.5em"
-              width="1.5em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-            </svg>
+            <a href="https://github.com/Petsamuel/Xpenzes">
+              <svg
+                stroke="currentColor"
+                fill="none"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="text-lg text-gray2 hover:text-black1 dark:hover:text-white cursor-pointer"
+                height="1.5em"
+                width="1.5em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+              </svg>
+            </a>
           </div>
-          <div className="" title="Support">
+          <div className="" title="Support Us">
             <a href="https://www.buymeacoffee.com/Bieefilled">
               {" "}
               <svg
